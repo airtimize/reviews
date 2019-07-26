2faa2066e7938fe51d3557d9fa4e1734aac39537
Dorris Wong <pingki@sbcglobal.net>
2019-07-26 01:32:10 -0700

    Make files eslint compliant

--- server/db/reviews_seed.js
+++ server/db/reviews_seed.js
@@ -1,10 +1,11 @@
 const faker = require('faker');
 const fs = require('fs');
+
 const fileStream = fs.createWriteStream('./reviews.csv');
 
 
-function something () {
-  return new Promise(resolve => {
+function something() {
+  return new Promise((resolve) => {
     fileStream.once('drain', resolve);
   });
 }
@@ -13,9 +14,9 @@ function something () {
 async function writer() {
   let ableToWrite = true;
 
-for (let i = 0; i < 50e6; i += 1) {
-
+  for (let i = 0; i < 50e6; i += 1) {
     const review_id = i + 1;
+
     const listing_id = faker.random.number({ min: 0, max: 1000000 });
     const guest_user_id = faker.random.number({ min: 0, max: 1000 });
     const review_text = faker.lorem.sentence();
@@ -29,19 +30,15 @@ for (let i = 0; i < 50e6; i += 1) {
     const currentDate = new Date();
     const end = currentDate.toISOString().slice(0, 10);
     const response_created_at = faker.date.between(start, end);
-    
-  ableToWrite = fileStream.write(`${review_id},${listing_id},${guest_user_id},${review_text},${reviews_created_at},${has_response},${host_user_id},${response_text},${response_created_at}\n`);
 
-  if (!ableToWrite) {
-    await something();
+    ableToWrite = fileStream.write(`${review_id},${listing_id},${guest_user_id},${review_text},${reviews_created_at},${has_response},${host_user_id},${response_text},${response_created_at}\n`);
+
+    if (!ableToWrite) {
+      await something();
     }
-
   }
 
   fileStream.end();
-
 }
 
 writer();
-
-
