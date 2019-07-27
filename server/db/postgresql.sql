4ad671eb201142bdbcaf8963a829997a215b10a8
Dorris Wong <pingki@sbcglobal.net>
2019-07-26 20:39:51 -0700

    Create postgresql schema file

---
+++ server/db/postgresql.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS airtimize_reviews;
+CREATE DATABASE airtimize_reviews;
+\connect airtimize_reviews;
+
+CREATE TABLE listings (
+  id SMALLINT,
+  accuracy FLOAT(1),
+  communication FLOAT(1),
+  cleanliness FLOAT(1),
+  location FLOAT(1),
+  checkin FLOAT(1),
+  value FLOAT(1),
+  num_reviews SMALLINT
+);
+
+CREATE TABLE reviews (
+  id SERIAL,
+  listing_id SMALLINT,
+  guest_user_id SMALLINT,
+  review_text VARCHAR(255),
+  review_created_at DATE,
+  host_user_id SMALLINT,
+  response_text VARCHAR(255),
+  response_created_at DATE
+);
+
+CREATE TABLE users (
+  id SERIAL,
+  avatar VARCHAR(30),
+  username VARCHAR(30)
+)
