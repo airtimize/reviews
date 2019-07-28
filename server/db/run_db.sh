4d333b316de7ec6119f0413bd587a239c7119c0c
Dorris Wong <pingki@sbcglobal.net>
2019-07-27 17:21:21 -0700

    Fix bash script for both schema loads

--- server/db/run_db.sh
+++ server/db/run_db.sh
@@ -4,5 +4,5 @@
 psql -h localhost -f schema/postgresql.sql
 # pv /tmp/data.txt | psql -c "COPY mytest FROM STDIN;"
 echo "Done with Postgres"
-#cqlsh -f schema/cassandra.cql
-#echo "Done with Cassandra"
+cqlsh -f schema/cassandra.cql
+echo "Done with Cassandra"
