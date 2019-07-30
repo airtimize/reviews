e967be90da49a4b06928b78304d5048afa1947e0
Dorris Wong <pingki@sbcglobal.net>
2019-07-29 20:52:03 -0700

    Implement TimeUUID

--- server/db/cassandra/query.sql
+++ server/db/cassandra/query.sql
@@ -1,6 +1,6 @@
 --POST
 INSERT INTO
-delete_reviews (review_id,
+reviews1 (review_id,
 listing_id,
 guest_username,
 guest_avatar,
@@ -16,7 +16,7 @@ host_username,
 host_avatar,
 response_text,
 response_created_at)
-VALUES (2,
+VALUES (now(),
 5,
 'helen',
 'img',
@@ -35,12 +35,13 @@ VALUES (2,
 
 --GET
 SELECT * FROM reviews WHERE listing_id = 2 ORDER BY review_created_at DESC;
+SELECT * FROM listings WHERE listing_id = 2;
 
 --PUT
-UPDATE delete_reviews SET guest_username = 'hellen',guest_avatar = 'imgasd',review_text = 'Aut dolor',review_created_at = 'Sat Mar 16 2019',accuracy = 2,communication = 2,cleanliness = 2,location = 2,checkin = 2,value = 2,host_username = 'hellen',host_avatar = 'img',response_text = 'Aut dolor',response_created_at = 'Sat Mar 16 2019' WHERE listing_id = 2 AND review_id =2;
+UPDATE delete_reviews SET guest_username = 'hellen',guest_avatar = 'imgasd',review_text = 'Aut dolor',review_created_at = 'Sat Mar 16 2019',accuracy = 2,communication = 2,cleanliness = 2,location = 2,checkin = 2,value = 2,host_username = 'hellen',host_avatar = 'img',response_text = 'Aut dolor',response_created_at = 'Sat Mar 16 2019' WHERE listing_id = 2 AND review_id = 077ae6c9-b27a-11e9-aee2-6839e69d3338;
 
 --PATCH
-UPDATE delete_reviews SET accuracy = 1 WHERE listing_id = 1 AND review_id = 1;
+UPDATE delete_reviews SET accuracy = 1 WHERE listing_id = 1 AND review_id = 077ae6c9-b27a-11e9-aee2-6839e69d3338;
 
 --DELETE
-DELETE FROM delete_reviews WHERE listing_id = 2 AND review_id = 1;
+DELETE FROM delete_reviews WHERE listing_id = 2 AND review_id = 077ae6c9-b27a-11e9-aee2-6839e69d3338;
--- server/db/postgres/query.sql
+++ server/db/postgres/query.sql
@@ -2,7 +2,8 @@
 INSERT INTO reviews (listing_id,guest_user_id,review_text,review_created_at,accuracy,communication,cleanliness,location,checkin,value,host_user_id,response_text,response_created_at) VALUES (5,270,'Aut dolor facilis et culpa.','Thu Aug 30 2018',3,4,4,5,1,1,738,'Ut similique quia deleniti ut ullam quia odit debitis.','Sat Mar 16 2019');
 
 --GET
-SELECT * FROM reviews WHERE listing_id = 2 ORDER BY review_created_at DESC;
+SELECT * FROM reviews WHERE listing_id = 999999 ORDER BY review_created_at DESC;
+SELECT * FROM reviews JOIN listings ON reviews.listing_id = listings.id WHERE listing_id = 999999 ORDER BY review_created_at DESC;
 
 --PUT
 UPDATE reviews SET listing_id = 2, guest_user_id = 2, review_text = 'Aut dolor', review_created_at = 'Sat Mar 16 2019',accuracy = 2, communication = 2,cleanliness = 2,location = 2,checkin = 2,value = 2,host_user_id = 3,response_text = 'Aut dolor',response_created_at = 'Sat Mar 16 2019' WHERE listing_id = 1 AND id = 1;
@@ -12,3 +13,13 @@ UPDATE reviews SET accuracy = 1 WHERE listing_id = 2 AND id = 1;
 
 --DELETE
 DELETE FROM reviews WHERE listing_id = 2 AND id = 1;
+
+EXPLAIN ANALYZE SELECT * FROM listings WHERE id=999999;
+EXPLAIN ANALYZE SELECT * FROM reviews WHERE listing_id = 999999 ORDER BY review_created_at DESC;
+EXPLAIN ANALYZE INSERT INTO reviews (listing_id,guest_user_id,review_text,review_created_at,accuracy,communication,cleanliness,location,checkin,value,host_user_id,response_text,response_created_at) VALUES (5,270,'Aut dolor facilis et culpa.','Thu Aug 30 2018',3,4,4,5,1,1,738,'Ut similique quia deleniti ut ullam quia odit debitis.','Sat Mar 16 2019');
+EXPLAIN ANALYZE UPDATE reviews SET listing_id = 2, guest_user_id = 2, review_text = 'Aut dolor', review_created_at = 'Sat Mar 16 2019',accuracy = 2, communication = 2,cleanliness = 2,location = 2,checkin = 2,value = 2,host_user_id = 3,response_text = 'Aut dolor',response_created_at = 'Sat Mar 16 2019' WHERE listing_id = 1 AND id = 1;
+EXPLAIN ANALYZE UPDATE reviews SET accuracy = 1 WHERE listing_id = 2 AND id = 1;
+EXPLAIN ANALYZE DELETE FROM reviews WHERE listing_id = 2 AND id = 1;
+
+CREATE INDEX idx_id ON listings (id);
+CREATE INDEX idx_reviews ON reviews (id);
